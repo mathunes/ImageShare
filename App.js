f468c4ab0154f2cb5f9b50bdfc3dafbb8e4a0a73
mathunes <matheusantunes720@gmail.com>
2021-11-25 00:56:04 -0300

    Custom README

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
+import * as SplashScreen from 'expo-splash-screen';
+
+import logo from './assets/logo.png';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 5000);
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    }
+
+  }
+
+  let openSharedDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openSharedDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Image source={logo} style={{ width: 305, height: 159 }} /> */}
+      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo} />
+
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+      
     </View>
   );
 }
@@ -16,6 +85,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "space-around"
   },
+  logo: {
+    width: 305,
+    height: 159,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    textAlign: "center"
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# image-share
+# ImageShare
+
+## Expo get started tutorial 
+
+### 🛠 Technologies
+
+- [Expo](https://expo.io/)
+- [React](https://pt-br.reactjs.org/)
+- [React Native](https://reactnative.dev/)
+
+### Features
+
+- [x] Pick image from local repository
+- [x] Share image
